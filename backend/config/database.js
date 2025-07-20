3082d6b715aa4f5c4ed55ed5e3f4d3e3d1337eb2
Elias Garcia <eliasgarcia48244@gmail.com>
2025-07-20 18:04:28 -0300

    a ver si funca

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,14 +1,11 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-const db = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "mysql"
-  }
-);
+dotenv.config();
+
+const db = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "mysql",
+  logging: false,
+});
 
 export default db;
